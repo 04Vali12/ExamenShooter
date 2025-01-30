7bf863a1aa9601f68aff845d9958237c4f303a61
Yaxkin y compañia <yrfloresd412@hotmail.com>
2025-01-29 19:53:51 -0600

    Commit Animacion Contador de balas

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
@@ -6,8 +7,52 @@ public class Gun : MonoBehaviour
     private GameObject _bullet;
     [SerializeField]
     private Transform _bulletPivot;
+    [SerializeField]
+    private Animator _weaponAnimator;
+    [SerializeField]
+    private int _maxBulletNumber = 20;
+    [SerializeField]
+    private int _cartridgeBulletsNumber = 5;
+    [SerializeField]
+    private int _TotalBulletsNumber = 0;
+    private int _currentBulletsNumber = 0;
+    private Text _bulletsText;
+
+
     public void Shoot()
     {
+        _weaponAnimator.Play("Shoot",-1,0f);
         GameObject.Instantiate(_bullet, _bulletPivot.position, _bulletPivot.rotation);
+        _currentBulletsNumber--;
+        UpdateBulletsTexts();
+    }
+    public void PickUpWeapon()
+    {
+        _TotalBulletsNumber = _maxBulletNumber;
+
+        Reload();
+        _weaponAnimator.Play("GetWeapon");
+        UpdateBulletsTexts();
+    }
+    public void Reload()
+    {
+        if (_TotalBulletsNumber > _cartridgeBulletsNumber)
+        {
+            _currentBulletsNumber = _cartridgeBulletsNumber;
+        } else if(_TotalBulletsNumber > 0)
+        {
+            _currentBulletsNumber = _TotalBulletsNumber;
+        }
+        _TotalBulletsNumber -= _currentBulletsNumber;
+        UpdateBulletsTexts();
+    
+    }
+    private void UpdateBulletsTexts()
+    {
+        if(_bulletsText == null)
+        {
+            _bulletsText = GameObject.Find("BulletsText").GetComponent<Text>();
+        }
+        _bulletsText.text = _currentBulletsNumber + "/" + _TotalBulletsNumber;
     }
 }
